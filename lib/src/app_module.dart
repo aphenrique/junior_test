4b0af7257776485d0268734f1663b47b286f80cc
Henrique <ap_henrique@hotmail.com>
2022-02-16 12:03:19 -0300

    Add Uno to core dependencies

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:fteam_test/src/core/utils/clients/uno_service.dart';
+import 'package:fteam_test/src/modules/photos_module.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind<Object>> get binds => [];
+  List<Bind<Object>> get binds => [Bind.lazySingleton((i) => UnoService())];
 
   @override
   List<ModularRoute> get routes => [
-  ];
+        ModuleRoute('/', module: PhotosModule()),
+      ];
 }
