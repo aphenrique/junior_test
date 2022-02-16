fa1666ae99177dfecb088a58848737a581686d4f
Henrique <ap_henrique@hotmail.com>
2022-02-16 12:05:17 -0300

    Add photo module dependencies

---
+++ lib/src/modules/photos_module.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:fteam_test/src/modules/walpapers/data/repositories/photo_repository_impl.dart';
+import 'package:fteam_test/src/modules/walpapers/domain/usecases/fetch_photos_usecase.dart';
+import 'package:fteam_test/src/modules/walpapers/external/api_photo_datasource.dart';
+import 'package:fteam_test/src/modules/walpapers/view/blocs/fetch_photos_bloc.dart';
+import 'package:fteam_test/src/modules/walpapers/view/pages/list_photos_page.dart';
+
+class PhotosModule extends Module {
+  @override
+  List<Bind<Object>> get binds => [
+        Bind.singleton((i) => ApiPhotoDatasource(i())),
+        Bind.singleton((i) => PhotoRepositoryImpl(i())),
+        Bind.singleton((i) => FetchPhotosUsecaseImpl(i())),
+        Bind.lazySingleton((i) => FetchPhotosBloc(i())),
+      ];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (_, __) => const ListPhotosPage()),
+      ];
+}
