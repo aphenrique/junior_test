5eac98da79d9c8d52195b078b0f61dbf92434f80
Henrique <ap_henrique@hotmail.com>
2022-02-16 12:14:23 -0300

    Implemented CustomSmallPhoto to photo grid

---
+++ lib/src/modules/walpapers/view/widgets/custom_small_photo_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomSmallPhotoWidget extends StatelessWidget {
+  final String photoPath;
+
+  const CustomSmallPhotoWidget({
+    Key? key,
+    required this.photoPath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(photoPath), fit: BoxFit.fitHeight),
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+}
