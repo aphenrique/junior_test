cb72706bf07b5ef7b8175b424850d67efcbe6d20
Henrique <ap_henrique@hotmail.com>
2022-02-17 16:41:35 -0300

    Add connection and photo params exceptions

--- lib/src/modules/walpapers/domain/erros/photo_exception.dart
+++ lib/src/modules/walpapers/domain/erros/photo_exception.dart
@@ -5,10 +5,18 @@ abstract class PhotoException implements Exception {
   PhotoException(this.message, [this.stackTrace]);
 }
 
+class PhotoParamsException extends PhotoException {
+  PhotoParamsException(String message) : super(message);
+}
+
 class PhotoRepositoryException extends PhotoException {
   PhotoRepositoryException(String message) : super(message);
 }
 
 class PhotoDatasourceException extends PhotoException {
   PhotoDatasourceException(String message) : super(message);
+}
+
+class LostInternetConnection extends PhotoException {
+  LostInternetConnection(String message) : super(message);
 }
