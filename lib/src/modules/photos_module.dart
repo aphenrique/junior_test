4a3d7f18d9c3589f5ac72e0b65d88050b1e6a209
Henrique <ap_henrique@hotmail.com>
2022-02-17 16:40:25 -0300

    Remove deleted dependency

--- lib/src/modules/photos_module.dart
+++ lib/src/modules/photos_module.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:fteam_test/src/modules/walpapers/data/repositories/photo_repository_impl.dart';
 import 'package:fteam_test/src/modules/walpapers/domain/usecases/fetch_photos_usecase.dart';
-import 'package:fteam_test/src/modules/walpapers/domain/usecases/search_photos_usecase.dart';
 import 'package:fteam_test/src/modules/walpapers/external/api_photo_datasource.dart';
+import 'package:fteam_test/src/modules/walpapers/view/blocs/photo_page_params.dart';
 import 'package:fteam_test/src/modules/walpapers/view/blocs/photos_bloc.dart';
 import 'package:fteam_test/src/modules/walpapers/view/pages/list_photos_page.dart';
 
@@ -12,8 +12,8 @@ class PhotosModule extends Module {
         Bind.singleton((i) => ApiPhotoDatasource(i())),
         Bind.singleton((i) => PhotoRepositoryImpl(i())),
         Bind.singleton((i) => FetchPhotosUsecaseImpl(i())),
-        Bind.singleton((i) => SearchPhotosUsecaseImpl(i())),
-        Bind.lazySingleton((i) => PhotosBloc(i(), i())),
+        Bind.lazySingleton((i) => PhotosBloc(i())),
+        Bind.singleton((i) => PhotoPageParams()),
       ];
 
   @override
