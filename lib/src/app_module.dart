ffd69fd16682a17f2c5e3fc98761ec195e7d4d53
Henrique <ap_henrique@hotmail.com>
2022-02-18 03:08:34 -0300

    Add photo detail route

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:fteam_test/src/core/utils/clients/uno_service.dart';
 import 'package:fteam_test/src/modules/photos_module.dart';
+import 'package:fteam_test/src/modules/walpapers/view/pages/photo_detail_page.dart';
 
 class AppModule extends Module {
   @override
@@ -9,5 +10,7 @@ class AppModule extends Module {
   @override
   List<ModularRoute> get routes => [
         ModuleRoute('/', module: PhotosModule()),
+        ChildRoute('/photoDetail',
+            child: (context, args) => PhotoDetailPage(imagePath: args.data)),
       ];
 }
