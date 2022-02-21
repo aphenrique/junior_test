1c2ba427e26047b8753bf0172899580ab1b60c7c
Henrique <ap_henrique@hotmail.com>
2022-02-21 15:31:52 -0300

    refactor tests

--- lib/src/core/utils/validators/url_validator.dart
+++ lib/src/core/utils/validators/url_validator.dart
@@ -1,7 +1,6 @@
 class UrlValidator {
   static bool isValid(String url) {
-    final regex = RegExp(
-        r"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-\/]+)+$");
+    final regex = RegExp(r"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-\/]+)+$");
 
     return regex.hasMatch(url);
   }
